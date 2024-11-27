f1fc83b0de52c22159421f1ee2551e29116a674b
oiivantsov <oiivantsov@outlook.com>
2024-11-27 23:31:59 +0200

    get all jobs available for all

--- backend/api-server-starter/routes/jobRouter.js
+++ backend/api-server-starter/routes/jobRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { getJobs, createJob, updateJob, deleteJob } = require("../controllers/jobControllers.js");
-const requireAuth = require("../middleware/requireAuth.js");
-  
+
 // get jobs route
 router.get("/", requireAuth, getJobs);
+
+// everyone can see the jobs, but only authenticated users can create, update,
+const requireAuth = require("../middleware/requireAuth.js");
   
 // create job route
 router.post("/create", requireAuth, createJob);
