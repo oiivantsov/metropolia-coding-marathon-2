0c3ee8da7491860001204534e99cf9cd7fb5f682
Tom <tomro9393@gmail.com>
2024-11-27 15:09:05 +0200

    Backend iteration 3 done

--- backend/api-server-starter/middleware/requireAuth.js
+++ backend/api-server-starter/middleware/requireAuth.js
@@ -16,6 +16,8 @@ const requireAuth = async (req, res, next) => {
 
   const token = authorization.split(" ")[1];
 
+  console.log(token);
+
   try {
     const { _id } = jwt.verify(token, process.env.SECRET);
 
